f491689c3f45d7c7ed8c0cb7673c049791396147
kesakesavan <kesa16102002@gmail.com>
2023-11-16 11:47:34 +0530

    scroll bar

--- script.js
+++ script.js
@@ -19,35 +19,3 @@ window.onscroll = () => {
 };    
 
 
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header ul li a');
-
-
-window.onscroll = () => {
-
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-
-        if(top >= offset && top < offset + height){
-
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header ul li a[href*='+id+']').classList.add('active');
-            });
-        };
-    });
-
-    //sticky header
-
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-    
-    // remove toggle icon and navbar when click navbar link scroll
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
